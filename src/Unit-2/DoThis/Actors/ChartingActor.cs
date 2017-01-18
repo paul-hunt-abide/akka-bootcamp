1919c3e1b4e242fdd8ccd978b150c64656a3bf3b
Paul Hunt <woodburysoft@gmail.com>
2017-01-18 13:45:30 +0000

    End of lesson 2.3

--- src/Unit-2/DoThis/Actors/ChartingActor.cs
+++ src/Unit-2/DoThis/Actors/ChartingActor.cs
@@ -8,6 +8,9 @@ namespace ChartApp.Actors
 {
     public class ChartingActor : ReceiveActor
     {
+        public const int MaxPoints = 250;
+        private int xPosCounter = 0;
+
         #region Messages
 
         public class InitializeChart
@@ -30,6 +33,16 @@ namespace ChartApp.Actors
             public Series Series { get; private set; }
         }
 
+        public class RemoveSeries
+        {
+            public RemoveSeries(string seriesName)
+            {
+                SeriesName = seriesName;
+            }
+
+            public string SeriesName { get; private set; }
+        }
+
         #endregion
 
         private readonly Chart _chart;
@@ -46,7 +59,9 @@ namespace ChartApp.Actors
 
             Receive<InitializeChart>(ic => HandleInitialize(ic));
             Receive<AddSeries>(addSeries => HandleAddSeries(addSeries));
-        }        
+            Receive<RemoveSeries>(removeSeries => HandleRemoveSeries(removeSeries));
+            Receive<Metric>(metric => HandleMetrics(metric));
+        }
 
         #region Individual Message Type Handlers
 
@@ -54,34 +69,92 @@ namespace ChartApp.Actors
         {
             if (ic.InitialSeries != null)
             {
-                //swap the two series out
                 _seriesIndex = ic.InitialSeries;
             }
 
-            //delete any existing series
             _chart.Series.Clear();
 
-            //attempt to render the initial chart
+            var area = _chart.ChartAreas[0];
+            area.AxisX.IntervalType = DateTimeIntervalType.Number;
+            area.AxisY.IntervalType = DateTimeIntervalType.Number;
+
+            SetChartBoundaries();
+
             if (_seriesIndex.Any())
             {
                 foreach (var series in _seriesIndex)
                 {
-                    //force both the chart and the internal index to use the same names
                     series.Value.Name = series.Key;
                     _chart.Series.Add(series.Value);
                 }
             }
+
+            SetChartBoundaries();
         }
 
         private void HandleAddSeries(AddSeries series)
         {
-            if (!string.IsNullOrEmpty(series.Series.Name) && !_seriesIndex.ContainsKey(series.Series.Name)                )
+            if (!string.IsNullOrEmpty(series.Series.Name) && !_seriesIndex.ContainsKey(series.Series.Name))
             {
                 _seriesIndex.Add(series.Series.Name, series.Series);
                 _chart.Series.Add(series.Series);
+                SetChartBoundaries();
+            }
+        }
+
+        private void HandleRemoveSeries(RemoveSeries series)
+        {
+            if (!string.IsNullOrEmpty(series.SeriesName) && _seriesIndex.ContainsKey(series.SeriesName))
+            {
+                var seriesToRemove = _seriesIndex[series.SeriesName];
+                _seriesIndex.Remove(series.SeriesName);
+                _chart.Series.Remove(seriesToRemove);
+                SetChartBoundaries();
+            }
+        }
+
+        private void HandleMetrics(Metric metric)
+        {
+            if (!string.IsNullOrEmpty(metric.Series) && _seriesIndex.ContainsKey(metric.Series))
+            {
+                var series = _seriesIndex[metric.Series];
+                if (series.Points == null) return;
+                series.Points.AddXY(xPosCounter++, metric.CounterValue);
+                while (series.Points.Count > MaxPoints)
+                {
+                    series.Points.RemoveAt(0);
+                }
+                SetChartBoundaries();
             }
         }
 
         #endregion
+
+        private void SetChartBoundaries()
+        {
+            double maxAxisX, maxAxisY, minAxisX, minAxisY = 0.0d;
+            var allPoints = _seriesIndex.Values.SelectMany(series => series.Points).ToList();
+            var yValues = allPoints.SelectMany(point => point.YValues).ToList();
+            maxAxisX = xPosCounter;
+            minAxisX = xPosCounter - MaxPoints;
+            maxAxisY = yValues.Count > 0 ? Math.Ceiling(yValues.Max()) : 1.0d;
+            minAxisY = yValues.Count > 0 ? Math.Floor(yValues.Min()) : 0.0d;
+
+            if (minAxisY == maxAxisY)
+            {
+                maxAxisY++;
+            }
+
+            var area = _chart.ChartAreas[0];
+
+            area.AxisY.Minimum = minAxisY;
+            area.AxisY.Maximum = Math.Max(1.0d, maxAxisY);
+
+            if (allPoints.Count > 2)
+            {                
+                area.AxisX.Minimum = minAxisX;
+                area.AxisX.Maximum = maxAxisX;                
+            }
+        }        
     }
 }
---
+++ src/Unit-2/DoThis/Actors/ChartingMessages.cs
@@ -0,0 +1,62 @@
+﻿using Akka.Actor;
+
+namespace ChartApp.Actors
+{
+    #region Reporting
+
+    public class GatherMetrics
+    {
+    }
+
+    public class Metric
+    {
+        public Metric(string series, float counterValue)
+        {
+            Series = series;
+            CounterValue = counterValue;
+        }
+
+        public string Series { get; private set; }
+
+        public float CounterValue { get; private set; }
+    }
+
+    #endregion
+
+    #region Performance Counter Management
+
+    public enum CounterType
+    {
+        Cpu,
+        Memory,
+        Disk
+    }
+
+    public class SubscribeCounter
+    {
+        public SubscribeCounter(CounterType counter, IActorRef subscriber)
+        {
+            Subscriber = subscriber;
+            Counter = counter;
+        }
+
+        public CounterType Counter { get; private set; }
+
+        public IActorRef Subscriber { get; private set; }
+    }
+
+    public class UnsubscribeCounter
+    {
+        public UnsubscribeCounter(CounterType counter, IActorRef subscriber)
+        {
+            Counter = counter;
+            Subscriber = subscriber;
+        }
+
+        public CounterType Counter { get; private set; }
+
+        public IActorRef Subscriber { get; private set; }
+    }
+
+    #endregion
+}
